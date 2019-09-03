42cee36766ec0410264eb716bfb45e268e5d1e2f
Martti Laine <martti@codeclown.net>
2019-09-03 15:44:57 +0300

    Implement GET /site-api/:tableName

--- src/server.js
+++ src/server.js
@@ -6,13 +6,27 @@ const server = ({ db }) => {
   const app = express();
 
   app.get('/site-api/:tableName', (req, res, next) => {
+    const queryableColumns = {
+      MemberOfParliament: ['firstname', 'lastname']
+    };
     const { tableName } = req.params;
     const table = tables.find(table => table.tableName === tableName);
     if (!table) {
-      return res.status(404).json({});
+      return res.status(404).json([]);
     }
-    return db(tableName).limit(10).then(rows => {
-      res.json(rows);
+    const { terms } = req.query;
+    if (typeof terms === 'undefined' || terms.length < 2) {
+      return res.status(401).json([]);
+    }
+    const query = db(tableName);
+    const columnsToQuery = queryableColumns[tableName] || [];
+    terms.trim().split(/\s+/g).forEach(term => {
+      columnsToQuery.forEach(column => {
+        query.orWhere(column, 'ilike', `%${term}%`);
+      });
+    });
+    return query.then(rows => {
+      res.set('x-terms', terms).json(rows);
     })
   });
 
--- test/app.test.js
+++ test/app.test.js
@@ -3,71 +3,125 @@ const { expect } = require('chai');
 const server = require('../src/server');
 const getDb = require('../src/db');
 
-const app = server();
+const record = {"personId":"102","lastname":" Aaltonen","firstname":"Markus","party":"","minister":"f","XmlData":null,"XmlDataSv":"<redacted>","XmlDataFi":"<redacted>"};
+// TODO should unify with bin/download-data
+record.personId_Integer = 102;
 
-describe('GET /site-api/edustajat', () => {
+describe('GET /site-api/MemberOfParliament', () => {
   let app, db;
   before(() => {
     db = getDb('testing');
     app = server({ db });
+    return db('MemberOfParliament').insert(record);
   });
-  after(() => db.destroy());
+  after(() => db('MemberOfParliament').truncate().then(() => db.destroy()));
 
-  it('returns query results', () => {
+  it('returns nothing if nothing matches', () => {
     return request(app)
-      .get('/site-api/edustajat?nameQuery=hakka')
+      .get('/site-api/MemberOfParliament?terms=foobar')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(response => {
-        expect(respone.body).to.be.an('array');
-        expect(respone.body).to.have.length(1);
-        expect(respone.body[0]).to.deep.equal({
-          firstName: 'Teuvo',
-          lastName: 'Hakkarainen'
-        });
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(0);
+      });
+  });
+
+  it('queries MemberOfParliament only based on name', () => {
+    return request(app)
+      .get(`/site-api/MemberOfParliament?terms=${encodeURIComponent(record.id)}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(0);
+      });
+  });
+
+  it('returns query results', () => {
+    return request(app)
+      .get('/site-api/MemberOfParliament?terms=aalto')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(1);
+        expect(response.body[0]).to.deep.equal(record);
+      });
+  });
+
+  it('returns query terms as header', () => {
+    return request(app)
+      .get('/site-api/MemberOfParliament?terms=aalto')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.headers['x-terms']).to.equal('aalto');
       });
   });
 
   it('requires at least 2 characters in search query', () => {
     return request(app)
-      .get('/site-api/edustajat?nameQuery=')
+      .get('/site-api/MemberOfParliament?terms=')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200)
+      .expect(401)
       .then(response => {
-        expect(respone.body).to.be.an('array');
-        expect(respone.body).to.have.length(0);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(0);
       })
       .then(() =>
         request(app)
-          .get('/site-api/edustajat?nameQuery=a')
+          .get('/site-api/MemberOfParliament?terms=a')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
-          .expect(200)
+          .expect(401)
           .then(response => {
-            expect(respone.body).to.be.an('array');
-            expect(respone.body).to.have.length(0);
+            expect(response.body).to.be.an('array');
+            expect(response.body).to.have.length(0);
           })
       )
       .then(() =>
         request(app)
-          .get('/site-api/edustajat?nameQuery=ak')
+          .get('/site-api/MemberOfParliament?terms=aa')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .then(response => {
-            expect(respone.body).to.be.an('array');
-            expect(respone.body).to.have.length(1);
-            expect(respone.body[0]).to.deep.equal({
-              firstName: 'Teuvo',
-              lastName: 'Hakkarainen'
-            });
+            expect(response.body).to.be.an('array');
+            expect(response.body).to.have.length(1);
+            expect(response.body[0]).to.deep.equal(record);
           })
       );
   });
 
-  it('is case insensitive');
+  it('is case insensitive', () => {
+    return request(app)
+      .get('/site-api/MemberOfParliament?terms=AALTO')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(1);
+        expect(response.body[0]).to.deep.equal(record);
+      });
+  });
 
-  it('supports wildcard');
+  it('does not care about extra whitespace', () => {
+    return request(app)
+      .get(`/site-api/MemberOfParliament?terms=${encodeURIComponent(' rkus  aalto')}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(1);
+        expect(response.body[0]).to.deep.equal(record);
+      });
+  });
 });
