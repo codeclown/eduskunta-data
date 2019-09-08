4bb9db8840aeb9c9851717a119fbef736a8e81ae
Martti Laine <martti@codeclown.net>
2019-09-08 15:56:20 +0300

    Respect NODE_ENV

--- src/server/db.js
+++ src/server/db.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const config = require('./knexfile');
 
 const getDb = env => {
-  return knex(config[env || 'development']);
+  return knex(config[env || process.env.NODE_ENV || 'development']);
 };
 
 module.exports = getDb;
